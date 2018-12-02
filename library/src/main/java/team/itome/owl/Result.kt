3a3350fd1efa4b2707d227f120de5f23c9786f35
itome <itometeam@gmail.com>
2018-12-02 20:31:54 +0900

    Add invoke function in Result

--- library/src/main/java/team/itome/owl/Result.kt
+++ library/src/main/java/team/itome/owl/Result.kt
@@ -5,6 +5,13 @@ sealed class Result<T> {
   data class Success<T>(val value: T) : Result<T>()
   data class Failure<T>(val error: Throwable) : Result<T>()
 
+  inline operator fun <A> invoke(f: () -> A): Result<A> =
+      try {
+        Success(f())
+      } catch (e: Throwable) {
+        Failure(e)
+      }
+
   companion object {
     fun <T> just(data: T): Result<T> = Success(data)
     fun <T> raise(error: Throwable): Result<T> = Failure(error)
